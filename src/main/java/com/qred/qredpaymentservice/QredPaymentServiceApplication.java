9f0171736c4d252d4ad4dfa8bf60e0cc39facaca
Hassan Sheikha <devazan@Hassans-MacBook-Pro.local>
2025-07-09 13:38:02 +0200

    initial commit

---
+++ src/main/java/com/qred/qredpaymentservice/QredPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.qred.qredpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QredPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QredPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qred/qredpaymentservice/QredPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qred.qredpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QredPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
